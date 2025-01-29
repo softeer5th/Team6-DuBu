d29158ca63610cbde3fc1549fbe512506d1cf8fe
young-jin-son <sdudwls128@naver.com>
2025-01-29 18:00:41 +0900

    feat: 랜딩 페이지 헤더 컴포넌트 구현 #9

---
+++ frontend/src/pages/LandingPage/components/LandingHeader/index.tsx
@@ -0,0 +1,11 @@
+import Icon from '@/components/Icon';
+
+const Header = () => {
+  return (
+    <header>
+      <Icon icon="Doreburn" width={110} height={14} />
+    </header>
+  );
+};
+
+export default Header;
