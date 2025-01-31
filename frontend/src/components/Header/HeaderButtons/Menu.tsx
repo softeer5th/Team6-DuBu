f2ef6b3b91195ff659031726d81d38bbfe5befd8
young-jin-son <sdudwls128@naver.com>
2025-02-01 01:05:16 +0900

    feat: 메뉴 버튼 클릭 시 Drawer 열기 기능 추가 #22

--- frontend/src/components/Header/HeaderButtons/Menu.tsx
+++ frontend/src/components/Header/HeaderButtons/Menu.tsx
@@ -1,11 +1,13 @@
-import { BUTTONS } from '../Header.constants';
-
+import { useDrawer } from '@/components/Drawer/hooks/useDrawer';
+import { BUTTONS } from '@/components/Header/Header.constants';
 import * as S from '@/components/Header/Header.styled';
 import Icon, { IconType } from '@/components/Icon';
 
 export const Menu = () => {
+  const { openDrawer } = useDrawer();
+
   return (
-    <S.MenuButton aria-label={BUTTONS.MENU.text}>
+    <S.MenuButton aria-label={BUTTONS.MENU.text} onClick={openDrawer}>
       <Icon icon={BUTTONS.MENU.icon as IconType} />
     </S.MenuButton>
   );
