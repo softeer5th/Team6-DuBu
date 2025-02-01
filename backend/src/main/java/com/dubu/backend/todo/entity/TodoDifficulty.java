e6f24dfa0f38b4c225223c2032c3bb8499ad33a0
khw7385 <khw7385@naver.com>
2025-02-01 17:15:20 +0900

    feat: TodoDifficulty 수정 #23 필드 이름 수정 name -> koreanName 필드 조회 메서드 추가 -> getName 필드 값으로 TodoDifficulty 객체를 찾기 위한 map 및 메서드 추가 -> TodoDifficulty, getByName()

--- backend/src/main/java/com/dubu/backend/todo/entity/TodoDifficulty.java
+++ backend/src/main/java/com/dubu/backend/todo/entity/TodoDifficulty.java
@@ -1,11 +1,30 @@
 package com.dubu.backend.todo.entity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum TodoDifficulty {
     EASY("쉬움"), NORMAL("보통"), HARD("어려움");
 
-    private String name;
+    private final String koreanName;
 
-    TodoDifficulty(String name) {
-        this.name = name;
+    TodoDifficulty(String koreanName) {
+        this.koreanName = koreanName;
+    }
+
+    public String getName(){
+        return this.koreanName;
+    }
+
+    private static final Map<String, TodoDifficulty> todoDifficultyMap = new HashMap<>();
+
+    static {
+        for (TodoDifficulty todoDifficulty : TodoDifficulty.values()) {
+            todoDifficultyMap.put(todoDifficulty.getName(), todoDifficulty);
+        }
+    }
+
+    public static TodoDifficulty getByName(String name){
+        return todoDifficultyMap.get(name);
     }
 }
