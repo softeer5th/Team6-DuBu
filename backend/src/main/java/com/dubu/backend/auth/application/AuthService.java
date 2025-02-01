8334142087ef068ed04c541bfeafa1edd2eebeda
codrin2 <hssang127@gmail.com>
2025-02-01 18:29:15 +0900

    feat: 인증 관련 AuthService 구현 #11

---
+++ backend/src/main/java/com/dubu/backend/auth/application/AuthService.java
@@ -0,0 +1,43 @@
+package com.dubu.backend.auth.application;
+
+import com.dubu.backend.auth.domain.OauthProvider;
+import com.dubu.backend.auth.domain.authcode.AuthCodeRequestUrlProviderComposite;
+import com.dubu.backend.auth.dto.TokenResponse;
+import com.dubu.backend.auth.infra.oauth.kakao.port.KakaoTokenPort;
+import com.dubu.backend.auth.infra.oauth.kakao.port.KakaoUserPort;
+import com.dubu.backend.member.domain.Member;
+import com.dubu.backend.member.infrastructure.repository.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final TokenService tokenService;
+    private final KakaoTokenPort kakaoTokenPort;
+    private final KakaoUserPort kakaoUserPort;
+    private final MemberRepository memberRepository;
+    private final AuthCodeRequestUrlProviderComposite authCodeRequestUrlProviderComposite;
+
+    public String getAuthCodeRequestUrl(OauthProvider oauthProvider) {
+        return authCodeRequestUrlProviderComposite.provide(oauthProvider);
+    }
+
+    public TokenResponse reissueToken(HttpServletRequest request) {
+        String newAccessToken = tokenService.reissue(request);
+        return new TokenResponse(newAccessToken);
+    }
+
+    public TokenResponse issueTokenAfterKakaoLogin(String code) {
+        String tokenFromKakao = kakaoTokenPort.getAccessTokenByCode(code);
+        Member kakaoUser = kakaoUserPort.findUserFromKakao(tokenFromKakao);
+        Member loginMember = memberRepository.findByProviderId(kakaoUser.getProviderId())
+                        .orElseGet(() -> memberRepository.save(kakaoUser));
+        String newAccessToken = tokenService.issue(loginMember.getId());
+
+        return new TokenResponse(newAccessToken);
+    }
+}
