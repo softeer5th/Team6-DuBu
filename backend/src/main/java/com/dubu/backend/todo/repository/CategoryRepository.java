9e268a5db41769e5680f6d668dc31ea3a4800e0e
khw7385 <khw7385@naver.com>
2025-02-01 17:17:08 +0900

    feat: CategoryRepository 추가 #23

---
+++ backend/src/main/java/com/dubu/backend/todo/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.dubu.backend.todo.repository;
+
+import com.dubu.backend.todo.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
+}
