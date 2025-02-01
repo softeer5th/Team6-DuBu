2b11192ae75ae66dda6114ef7c47a75ba7cc6f90
Gyuhan Park <rbgks1937@naver.com>
2025-02-01 18:27:48 +0900

    feat: 할 일 API를 연동하여 할 일 리스트 데이터 구조화 #21

--- frontend/src/pages/EditPage/components/TodoTab/index.tsx
+++ frontend/src/pages/EditPage/components/TodoTab/index.tsx
@@ -2,8 +2,14 @@ import * as S from './TodoTab.styled';
 
 import IconButton from '@/components/Button/IconButton';
 import Icon from '@/components/Icon';
+import useQueryParamsDate from '@/hooks/useQueryParamsDate';
+import useTodoListQuery from '@/hooks/useTodoListQuery';
+import { categoryMapper } from '@/pages/EditPage/EditPage.constants';
 
 const TodoTab = () => {
+  const { isToday } = useQueryParamsDate();
+  const { data: todoList } = useTodoListQuery(isToday);
+
   return (
     <S.TodoTabLayout>
       <S.SloganWrapper>
@@ -12,41 +18,21 @@ const TodoTab = () => {
       </S.SloganWrapper>
 
       <S.TodoEditList>
-        <S.TodoEditItem>
-          <IconButton icon={<Icon icon="MinusCircle" />} />
-          <S.TodoTextWrapper>
-            <S.TodoTitle>오디오북 듣기</S.TodoTitle>
-            <S.TodoBadgeWrapper>
-              <S.TodoBadge icon="Reading">#독서</S.TodoBadge>
-              <S.TodoBadge>#쉬움</S.TodoBadge>
-            </S.TodoBadgeWrapper>
-          </S.TodoTextWrapper>
-          <IconButton icon={<Icon icon="Edit" />} />
-        </S.TodoEditItem>
-
-        <S.TodoEditItem>
-          <IconButton icon={<Icon icon="MinusCircle" />} />
-          <S.TodoTextWrapper>
-            <S.TodoTitle>AI와 대화 연습하기</S.TodoTitle>
-            <S.TodoBadgeWrapper>
-              <S.TodoBadge icon="Language">#제2외국어</S.TodoBadge>
-              <S.TodoBadge>#보통</S.TodoBadge>
-            </S.TodoBadgeWrapper>
-          </S.TodoTextWrapper>
-          <IconButton icon={<Icon icon="Edit" />} />
-        </S.TodoEditItem>
-
-        <S.TodoEditItem>
-          <IconButton icon={<Icon icon="MinusCircle" />} />
-          <S.TodoTextWrapper>
-            <S.TodoTitle>나만의 음악 플레이리스트 만들기</S.TodoTitle>
-            <S.TodoBadgeWrapper>
-              <S.TodoBadge icon="Hobby">#취미</S.TodoBadge>
-              <S.TodoBadge>#어려움</S.TodoBadge>
-            </S.TodoBadgeWrapper>
-          </S.TodoTextWrapper>
-          <IconButton icon={<Icon icon="Edit" />} />
-        </S.TodoEditItem>
+        {todoList?.map((todo) => (
+          <S.TodoEditItem key={todo.todo_id}>
+            <IconButton icon={<Icon icon="MinusCircle" />} />
+            <S.TodoTextWrapper>
+              <S.TodoTitle>{todo.name}</S.TodoTitle>
+              <S.TodoBadgeWrapper>
+                <S.TodoBadge $category={todo.category}>
+                  {`#${categoryMapper[todo.category]}`}
+                </S.TodoBadge>
+                <S.TodoBadge>{`#${todo.difficulty}`}</S.TodoBadge>
+              </S.TodoBadgeWrapper>
+            </S.TodoTextWrapper>
+            <IconButton icon={<Icon icon="Edit" />} />
+          </S.TodoEditItem>
+        ))}
         <IconButton icon={<Icon icon="PlusCircle" />} text="직접 추가하기" isFull={true} />
       </S.TodoEditList>
     </S.TodoTabLayout>
