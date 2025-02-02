c1c5d5761054a81ef2fd847bee5fd517a5e9f2a0
khw7385 <khw7385@naver.com>
2025-02-02 15:28:56 +0900

    feat: TodoType 객체 수정 및 탐색 메서드 구현 #23 DONE -> 한 일 SCHEDULED -> 오늘 할 일, 내일 할 일

--- backend/src/main/java/com/dubu/backend/todo/entity/TodoType.java
+++ backend/src/main/java/com/dubu/backend/todo/entity/TodoType.java
@@ -1,5 +1,16 @@
 package com.dubu.backend.todo.entity;
 
 public enum TodoType {
-    SAVE, RECOMMEND, OTHER
+    SAVE, RECOMMEND, DONE, SCHEDULED;
+
+    public static TodoType get(String type){
+        if(type.equals("today") || type.equals("tomorrow"))
+            return TodoType.SCHEDULED;
+        else if(type.equals("save"))
+            return TodoType.SAVE;
+        else if(type.equals("recommend"))
+            return TodoType.RECOMMEND;
+        else
+            return TodoType.DONE;
+    }
 }
