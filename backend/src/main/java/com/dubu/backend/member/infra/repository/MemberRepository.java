c91ea64881534e9f6fc191cf11a2fb6578ebf5d9
codrin2 <hssang127@gmail.com>
2025-02-02 22:36:46 +0900

    fix: PK타입 Long으로 수정 #29

--- backend/src/main/java/com/dubu/backend/member/infra/repository/MemberRepository.java
+++ backend/src/main/java/com/dubu/backend/member/infra/repository/MemberRepository.java
@@ -1,10 +1,10 @@
-package com.dubu.backend.member.infrastructure.repository;
+package com.dubu.backend.member.infra.repository;
 
 import com.dubu.backend.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Integer> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByProviderId(String providerId);
 }
