16939c799164cd31a80509645b8098995d1c9a0f
khw7385 <khw7385@naver.com>
2025-02-02 16:16:34 +0900

    feat: Todo 를 조회할 때 Category 를 조인해서 가져오는 메서드 추가 #23

--- backend/src/main/java/com/dubu/backend/todo/repository/TodoRepository.java
+++ backend/src/main/java/com/dubu/backend/todo/repository/TodoRepository.java
@@ -2,8 +2,14 @@ package com.dubu.backend.todo.repository;
 
 import com.dubu.backend.todo.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long>{
+    @Query("SELECT t FROM Todo t JOIN t.category c WHERE t.id = :todoId")
+    Optional<Todo> findByIdWithCategory(@Param("todoId") Long todoId);
 }
