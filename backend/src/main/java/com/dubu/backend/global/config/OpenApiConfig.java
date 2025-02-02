c5b41e2a3c0e5c68a26f49be6902c5ecc6875dee
codrin2 <hssang127@gmail.com>
2025-02-02 22:41:15 +0900

    feat: Swagger에서 AccessToken을 적용할 수 있도록 설정 #29

---
+++ backend/src/main/java/com/dubu/backend/global/config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package com.dubu.backend.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "AccessToken";
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .info(new Info().title("API Documentation")
+                        .description("API 문서")
+                        .version("1.0"));
+    }
+}
