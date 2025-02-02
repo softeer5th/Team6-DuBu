0b04e4007f16d9b48736f32c072dc1a2d1587cb2
khw7385 <khw7385@naver.com>
2025-02-02 14:25:58 +0900

    refactor: Todo 생성 응답 dto 이름 변경 #23

---
+++ backend/src/main/java/com/dubu/backend/todo/dto/response/CreateTodoResponse.java
@@ -0,0 +1,15 @@
+package com.dubu.backend.todo.dto.response;
+
+
+import com.dubu.backend.todo.entity.Todo;
+import com.dubu.backend.todo.entity.TodoDifficulty;
+
+public record CreateTodoResponse(Long todoId, String category, String difficulty, String title) {
+    public CreateTodoResponse(Long todoId, String category, TodoDifficulty todoDifficulty, String title) {
+        this(todoId, category, todoDifficulty.getName(), title);
+    }
+
+    public static CreateTodoResponse fromEntity(Todo todo){
+        return new CreateTodoResponse(todo.getId(), todo.getCategory().getName(), todo.getTodoDifficulty(), todo.getTitle());
+    }
+}
