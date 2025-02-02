c1bfd8fc3c62fbed19f781b9a4a0c8fc3cc69b31
Gyuhan Park <rbgks1937@naver.com>
2025-02-03 02:59:29 +0900

    refactor: 즐겨찾기 쿼리키 상수 적용 #31

--- frontend/src/pages/EditPage/hooks/useFavoriteListQuery.ts
+++ frontend/src/pages/EditPage/hooks/useFavoriteListQuery.ts
@@ -1,10 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { getFavoriteTodoList } from '@/api/todo';
+import { QUERY_KEY } from '@/constants/queryKey';
 
 const useFavoriteTodoListQuery = () => {
   return useQuery({
-    queryKey: ['favorite'],
+    queryKey: [QUERY_KEY.favorite],
     queryFn: getFavoriteTodoList,
     staleTime: Infinity,
   });
