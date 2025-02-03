f3576e0c2d1c6004e8dd1505bdd0ec4c49dc70b5
khw7385 <khw7385@naver.com>
2025-02-03 16:31:37 +0900

    fix: TodoDifficulty 수정 #23 상수 객체만 남기기

--- backend/src/main/java/com/dubu/backend/todo/entity/TodoDifficulty.java
+++ backend/src/main/java/com/dubu/backend/todo/entity/TodoDifficulty.java
@@ -1,30 +1,6 @@
 package com.dubu.backend.todo.entity;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public enum TodoDifficulty {
-    EASY("쉬움"), NORMAL("보통"), HARD("어려움");
-
-    private final String koreanName;
-
-    TodoDifficulty(String koreanName) {
-        this.koreanName = koreanName;
-    }
-
-    public String getName(){
-        return this.koreanName;
-    }
-
-    private static final Map<String, TodoDifficulty> todoDifficultyMap = new HashMap<>();
-
-    static {
-        for (TodoDifficulty todoDifficulty : TodoDifficulty.values()) {
-            todoDifficultyMap.put(todoDifficulty.getName(), todoDifficulty);
-        }
-    }
-
-    public static TodoDifficulty getByName(String name){
-        return todoDifficultyMap.get(name);
-    }
+    EASY, NORMAL, HARD;
 }
