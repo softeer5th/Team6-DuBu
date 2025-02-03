202cf0246d17603a5bd4d495b02acf2deae1fc35
khw7385 <khw7385@naver.com>
2025-02-03 23:20:44 +0900

    feat: 할 일 데이터 조회 쿼리 추가 #23 parent todo 와 스케줄 날짜 기반 할 일 조회 parent todo 와 스케줄 기반 할 일 조회

--- backend/src/main/java/com/dubu/backend/todo/repository/TodoRepository.java
+++ backend/src/main/java/com/dubu/backend/todo/repository/TodoRepository.java
@@ -1,15 +1,23 @@
 package com.dubu.backend.todo.repository;
 
+import com.dubu.backend.todo.entity.Schedule;
 import com.dubu.backend.todo.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @Repository
-public interface TodoRepository extends JpaRepository<Todo, Long>{
+public interface TodoRepository extends JpaRepository<Todo, Long>, CustomTodoRepository{
     @Query("SELECT t FROM Todo t JOIN t.category c WHERE t.id = :todoId")
     Optional<Todo> findByIdWithCategory(@Param("todoId") Long todoId);
+
+    @Query("SELECT t FROM Todo t JOIN t.schedule s WHERE t.parentTodo = :todo AND s.date = :date")
+    Optional<Todo> findByParentTodoAndScheduleDate(@Param("todo") Todo parentTodo, @Param("date")LocalDate date);
+
+    @Query("SELECT t FROM Todo t WHERE t.parentTodo = :parentTodo AND t.schedule = :schedule")
+    Optional<Todo> findByParentTodoAndSchedule(@Param("parentTodo") Todo parentTodo, @Param("schedule") Schedule schedule);
 }
