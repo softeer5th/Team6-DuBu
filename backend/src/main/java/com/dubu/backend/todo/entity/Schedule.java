fc2628ae851cd1b55aa2e35306e7b7f8f7ca14dd
khw7385 <khw7385@naver.com>
2025-02-03 16:30:17 +0900

    fix: Schedule 엔티티 수정 #23 Todo 와의 연관관계 추가 Member 와의 연관관계 추가 Schedule 객체 생성하는 메서드 추가 -> of

--- backend/src/main/java/com/dubu/backend/todo/entity/Schedule.java
+++ backend/src/main/java/com/dubu/backend/todo/entity/Schedule.java
@@ -1,10 +1,13 @@
 package com.dubu.backend.todo.entity;
 
 import com.dubu.backend.global.domain.BaseTimeEntity;
+import com.dubu.backend.member.domain.Member;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -17,6 +20,20 @@ public class Schedule extends BaseTimeEntity {
     @Column(name = "schedule_id")
     private Long id;
 
-    @Column(name = "date", columnDefinition = "DATE")
+    @Column(name = "date", columnDefinition = "DATE", nullable = false)
     private LocalDate date;
+
+    @OneToMany(mappedBy = "schedule", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Todo> todos = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    public static Schedule of(LocalDate date, Member member){
+        return Schedule.builder()
+                .date(date)
+                .member(member)
+                .build();
+    }
 }
