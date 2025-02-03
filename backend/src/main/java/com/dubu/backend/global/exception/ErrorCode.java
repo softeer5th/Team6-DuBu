75327a83a1049691999c0fa2ef042f49e5b1790b
khw7385 <khw7385@naver.com>
2025-02-03 23:15:38 +0900

    feat: 즐겨찾기 혹은 추천에서 선택하여 오늘/내일/즐겨찾기 할 일 추가 할 때 이미 추가된 데이터에 대한 예외 및 에러 코드 작성 #23

--- backend/src/main/java/com/dubu/backend/global/exception/ErrorCode.java
+++ backend/src/main/java/com/dubu/backend/global/exception/ErrorCode.java
@@ -40,6 +40,8 @@ public enum ErrorCode {
     // Todo
     TODO_NOT_FOUND(NOT_FOUND, "해당 할 일이 존재하지 않습니다."),
 
+    ALREADY_ADDED_TODO(BAD_REQUEST, "이미 추가된 할 일 입니다."),
+
     // Schedule
     SCHEDULE_NOT_FOUND(NOT_FOUND, "스케줄을 찾을 수 없습니다.")
     ;
