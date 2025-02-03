c3d1058d9c07d842e4140853cb9aaf838b72ea9d
codrin2 <hssang127@gmail.com>
2025-02-03 23:56:12 +0900

    feat(PlaceService): 주소 검색 서비스 로직 구현 #32

---
+++ backend/src/main/java/com/dubu/backend/member/application/PlaceService.java
@@ -0,0 +1,28 @@
+package com.dubu.backend.member.application;
+
+import com.dubu.backend.member.dto.NaverPlaceApiResponse;
+import com.dubu.backend.member.dto.SearchPlaceResponse;
+import com.dubu.backend.member.infra.place.NaverPlaceApiClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PlaceService {
+    private final NaverPlaceApiClient naverPlaceApiClient;
+
+    public List<SearchPlaceResponse> searchPlaces(String query) {
+        NaverPlaceApiResponse response = naverPlaceApiClient.searchPlaces(query);
+        return response.places().stream()
+                .map(place -> new SearchPlaceResponse(
+                                place.title().replace("<b>", "").replace("</b>", ""),
+                                place.roadAddress(),
+                                place.mapx() / 1_000_0000.0,
+                                place.mapy() / 1_000_0000.0
+                ))
+                .collect(Collectors.toList());
+    }
+}
