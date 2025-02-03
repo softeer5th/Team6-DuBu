eb3de6e537a28691cbf2f374fdcd6404b72aa1e0
khw7385 <khw7385@naver.com>
2025-02-03 16:47:42 +0900

    feat: 저장된 할 일으로부터 할 일 생성 비즈니스 로직 구현 #23

--- backend/src/main/java/com/dubu/backend/todo/service/TodoManagementService.java
+++ backend/src/main/java/com/dubu/backend/todo/service/TodoManagementService.java
@@ -1,8 +1,11 @@
 package com.dubu.backend.todo.service;
 
+import com.dubu.backend.todo.dto.request.CreateTodoFromArchivedRequest;
 import com.dubu.backend.todo.dto.request.CreateTodoRequest;
 import com.dubu.backend.todo.dto.response.CreateTodoResponse;
 
 public interface TodoManagementService {
-    CreateTodoResponse createTodo(Long memberId, CreateTodoRequest createTodoRequest);
+    CreateTodoResponse createTodo(Long memberId, String todoType, CreateTodoRequest createTodoRequest);
+    CreateTodoResponse createTodoFromArchived(Long memberId, String todoType, CreateTodoFromArchivedRequest todoCreateRequest);
+
 }
--- backend/src/main/java/com/dubu/backend/todo/service/impl/TodoManagementServiceImpl.java
+++ backend/src/main/java/com/dubu/backend/todo/service/impl/TodoManagementServiceImpl.java
@@ -3,6 +3,7 @@ package com.dubu.backend.todo.service.impl;
 import com.dubu.backend.member.domain.Member;
 import com.dubu.backend.member.exception.NotFoundMemberException;
 import com.dubu.backend.member.infrastructure.repository.MemberRepository;
+import com.dubu.backend.todo.dto.request.CreateTodoFromArchivedRequest;
 import com.dubu.backend.todo.dto.request.CreateTodoRequest;
 import com.dubu.backend.todo.dto.response.CreateTodoResponse;
 import com.dubu.backend.todo.entity.Category;
@@ -57,4 +58,29 @@ public class TodoManagementServiceImpl implements TodoManagementService {
         return CreateTodoResponse.fromEntity(savedTodo);
     }
 
+    @Override
+    public CreateTodoResponse createTodoFromArchived(Long memberId, String todoType, CreateTodoFromArchivedRequest todoCreateRequest) {
+        Member member = memberRepository.findById(memberId).orElseThrow(NotFoundMemberException::new);
+        Todo parentTodo = todoRepository.findByIdWithCategory(todoCreateRequest.todoId()).orElseThrow(NotFoundTodoException::new);
+
+        // 내일 할 일 데이터 생성의 경우 내일 스케줄이 있는지를 조회하고 없으면 생성한다.
+        if(todoType.equals("tomorrow")){
+            boolean hasSchedule = scheduleRepository.findScheduleByMemberAndDate(member, ScheduledDateResolver.resolveScheduledDate(todoType)).isPresent();
+
+            if(!hasSchedule){
+                Schedule newSchedule = Schedule.of(LocalDate.now().plusDays(1), member);
+                scheduleRepository.save(newSchedule);
+            }
+        }
+        Schedule schedule = null;
+
+        // 오늘 할 일 혹은 내일 할 일 생성의 경우 최신의 스케줄을 조회한다.
+        if(todoType.equals("today") || todoType.equals("tomorrow")){
+            schedule = scheduleRepository.findFirstScheduleByMemberAndDateOrderByDateDesc(member, ScheduledDateResolver.resolveScheduledDate(todoType)).orElseThrow(NotFoundScheduleException::new);
+        }
+        Todo newTodo = Todo.of(parentTodo.getTitle(), parentTodo.getType(), parentTodo.getDifficulty(), parentTodo.getMemo(), member, parentTodo.getCategory(), parentTodo, schedule);
+        Todo savedTodo = todoRepository.save(newTodo);
+
+        return CreateTodoResponse.fromEntity(savedTodo);
+    }
 }
