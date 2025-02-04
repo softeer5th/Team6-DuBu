f81c01b12b911ab12feecd0ea52be532d3d88ad8
young-jin-son <sdudwls128@naver.com>
2025-02-04 14:20:03 +0900

    refactor: SVG 속성 카멜케이스로 변경 #25

--- frontend/src/components/Icon/icons/Alert.tsx
+++ frontend/src/components/Icon/icons/Alert.tsx
@@ -13,14 +13,14 @@ const Alert = (props: SVGProps<SVGSVGElement>) => {
       <path
         d="M10.5002 15.9993C13.8137 15.9993 16.4998 13.3132 16.4998 9.99967C16.4998 6.68615 13.8137 4 10.5002 4C7.18663 4 4.50049 6.68615 4.50049 9.99967C4.50049 13.3132 7.18663 15.9993 10.5002 15.9993Z"
         stroke="#656565"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M10.5 12.3993V9.99948M10.5 7.59961H10.506"
         stroke="#656565"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
