3d492aebe85e1d8431c490f875a202eb1f226cef
codrin2 <hssang127@gmail.com>
2025-02-04 10:16:57 +0900

    feat: 멤버의 저장된 학교,집 주소 조회 repository 구현 #39

--- backend/src/main/java/com/dubu/backend/member/infra/repository/AddressRepository.java
+++ backend/src/main/java/com/dubu/backend/member/infra/repository/AddressRepository.java
@@ -3,5 +3,8 @@ package com.dubu.backend.member.infra.repository;
 import com.dubu.backend.member.domain.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AddressRepository extends JpaRepository<Address, Long> {
+    List<Address> findByMemberId(Long memberId);
 }
