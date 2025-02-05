8406c5262b75d45abb93a2530f452b1f6d432bd2
khw7385 <khw7385@naver.com>
2025-02-05 14:47:35 +0900

    fix: TodoType 을 찾는 메소드 수정 #15 type 문자열의 대문자로 비교 tomorrow -> TOMORROW

--- backend/src/main/java/com/dubu/backend/todo/entity/TodoType.java
+++ backend/src/main/java/com/dubu/backend/todo/entity/TodoType.java
@@ -4,8 +4,8 @@ public enum TodoType {
     SAVE, RECOMMEND, DONE, SCHEDULED;
 
     public static TodoType get(String type){
-        return switch(type){
-            case "TODAY", "tomorrow" -> TodoType.SCHEDULED;
+        return switch(type.toUpperCase()){
+            case "TODAY", "TOMORROW" -> TodoType.SCHEDULED;
             case "SAVE" -> TodoType.SAVE;
             case "RECOMMEND" -> TodoType.RECOMMEND;
             default -> TodoType.DONE;
