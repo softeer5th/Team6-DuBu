abaf56156eb1a08ccd134443349760eaf6e60d65
khw7385 <khw7385@naver.com>
2025-02-05 14:45:57 +0900

    feat: member 로 필터링 MemberCategory 조회 #15 category_id 만 반환

--- backend/src/main/java/com/dubu/backend/member/infra/repository/MemberCategoryRepository.java
+++ backend/src/main/java/com/dubu/backend/member/infra/repository/MemberCategoryRepository.java
@@ -1,7 +1,14 @@
 package com.dubu.backend.member.infra.repository;
 
+import com.dubu.backend.member.domain.Member;
 import com.dubu.backend.member.domain.MemberCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MemberCategoryRepository extends JpaRepository<MemberCategory, Long> {
+    @Query("SELECT mc.category.id FROM MemberCategory mc WHERE mc.member = :member")
+    List<Long> findByMember(@Param("member") Member member);
 }
