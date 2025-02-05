acacc1bf0cc619802b58a252f9b3b3d99983d78e
khw7385 <khw7385@naver.com>
2025-02-05 14:41:16 +0900

    feat: TodoRepository 쿼리 추가 #15 category_id 로 필터링 Todo List 조회 쿼리 schedule 로 필터링 category fetch join 한 Todo List 조회 쿼리

--- backend/src/main/java/com/dubu/backend/todo/repository/TodoRepository.java
+++ backend/src/main/java/com/dubu/backend/todo/repository/TodoRepository.java
@@ -5,18 +5,24 @@ import com.dubu.backend.todo.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long>{
     @Query("SELECT t FROM Todo t JOIN t.category c WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithCategory(@Param("todoId") Long todoId);
+    Optional<Todo> findByIdWithCategory(@Param("todoId")Long todoId);
 
     @Query("SELECT t FROM Todo t JOIN t.schedule s WHERE t.parentTodo = :todo AND s.date = :date")
-    Optional<Todo> findByParentTodoAndScheduleDate(@Param("todo") Todo parentTodo, @Param("date")LocalDate date);
+    Optional<Todo> findByParentTodoAndScheduleDate(@Param("todo")Todo parentTodo, @Param("date")LocalDate date);
 
     @Query("SELECT t FROM Todo t WHERE t.parentTodo = :parentTodo AND t.schedule = :schedule")
-    Optional<Todo> findByParentTodoAndSchedule(@Param("parentTodo") Todo parentTodo, @Param("schedule") Schedule schedule);
+    Optional<Todo> findByParentTodoAndSchedule(@Param("parentTodo")Todo parentTodo, @Param("schedule")Schedule schedule);
+
+    @Query("SELECT t FROM Todo t WHERE t.category.id in :categoryIds")
+    List<Todo> findTodosByCategoryIds(@Param("categoryIds")List<Long> categoryIds);
+
+    @Query("SELECT t FROM Todo t join fetch t.category WHERE t.schedule = :schedule")
+    List<Todo> findTodosBySchedule(@Param("schedule")Schedule schedule);
 }
