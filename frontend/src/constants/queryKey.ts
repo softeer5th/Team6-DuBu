16ee6009264880f8c24570639e0d060f3f7da206
Gyuhan Park <rbgks1937@naver.com>
2025-02-06 02:22:45 +0900

    feat: recommendTodoFilterQuery 구현 #44

--- frontend/src/constants/queryKey.ts
+++ frontend/src/constants/queryKey.ts
@@ -2,4 +2,6 @@ export const QUERY_KEY = {
   todoList: 'todoList',
   favorite: 'favorite',
   recommend: 'recommend',
+  recommendLimit: 'recommendLimit',
+  recommendAll: 'recommendAll',
 } as const;
