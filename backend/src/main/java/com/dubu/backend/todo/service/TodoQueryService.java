54eb2f5c3656de9145d08ccb053a10628d3210aa
khw7385 <khw7385@naver.com>
2025-02-05 18:37:24 +0900

    feat: 즐겨찾기 조회 비즈니스 로직 구현 #47

--- backend/src/main/java/com/dubu/backend/todo/service/TodoQueryService.java
+++ backend/src/main/java/com/dubu/backend/todo/service/TodoQueryService.java
@@ -1,5 +1,7 @@
 package com.dubu.backend.todo.service;
 
+import com.dubu.backend.global.domain.PageResponse;
+import com.dubu.backend.todo.dto.request.SaveTodoQueryRequest;
 import com.dubu.backend.todo.dto.response.TodoInfo;
 
 import java.util.List;
@@ -7,4 +9,5 @@ import java.util.List;
 public interface TodoQueryService {
     List<TodoInfo> findTodayTodos(Long memberId);
     List<TodoInfo> findTomorrowTodos(Long memberId);
+    PageResponse<List<TodoInfo>> findFavoritesTodos(Long memberId, SaveTodoQueryRequest request);
 }
