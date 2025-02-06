50e08a20396c789076b859dc0758a830751fafc7
codrin2 <hssang127@gmail.com>
2025-02-06 17:55:35 +0900

    feat: Plan 생성 비즈니스 로직 구현 #53

---
+++ backend/src/main/java/com/dubu/backend/plan/application/PlanService.java
@@ -0,0 +1,38 @@
+package com.dubu.backend.plan.application;
+
+import com.dubu.backend.member.domain.Member;
+import com.dubu.backend.member.exception.MemberNotFoundException;
+import com.dubu.backend.member.infra.repository.MemberRepository;
+import com.dubu.backend.plan.domain.Path;
+import com.dubu.backend.plan.domain.Plan;
+import com.dubu.backend.plan.dto.request.PlanSaveRequest;
+import com.dubu.backend.plan.infra.repository.PathRepository;
+import com.dubu.backend.plan.infra.repository.PlanRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class PlanService {
+    private final MemberRepository memberRepository;
+    private final PlanRepository planRepository;
+    private final PathRepository pathRepository;
+
+    @Transactional
+    public void savePlan(Long memberId, PlanSaveRequest planSaveRequest) {
+        Member currentMember = memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberNotFoundException(memberId));
+
+        Plan newPlan = Plan.createPlan(currentMember, planSaveRequest.totalSectionTime());
+        planRepository.save(newPlan);
+
+        List<Path> paths = planSaveRequest.paths().stream()
+                .map(pathRequest -> Path.createPath(newPlan, pathRequest))
+                .toList();
+        pathRepository.saveAll(paths);
+    }
+}
