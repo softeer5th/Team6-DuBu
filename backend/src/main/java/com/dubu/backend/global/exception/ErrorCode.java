b938ff2aa25064739608dfce7f515d21f30cd349
codrin2 <hssang127@gmail.com>
2025-02-06 17:50:48 +0900

    feat: INVALID_TRAFFIC_TYPE 에러코드 추가 #53

--- backend/src/main/java/com/dubu/backend/global/exception/ErrorCode.java
+++ backend/src/main/java/com/dubu/backend/global/exception/ErrorCode.java
@@ -40,11 +40,12 @@ public enum ErrorCode {
     // Category
     CATEGORY_NOT_FOUND(NOT_FOUND, "카테고리를 찾을 수 없습니다. categoryName : %s"),
 
+    // Path
+    INVALID_TRAFFIC_TYPE(BAD_REQUEST, "지원하지 않는 대중교통 형식입니다. trafficType : %s"),
+
     // Todo
     TODO_NOT_FOUND(NOT_FOUND, "해당 할 일이 존재하지 않습니다."),
-
     ALREADY_ADDED_TODO(BAD_REQUEST, "이미 추가된 할 일 입니다."),
-
     TODO_LIMIT_EXCEEDED(BAD_REQUEST, "할 일은 최대 3개까지 추가할 수 있습니다."),
 
     // Schedule
