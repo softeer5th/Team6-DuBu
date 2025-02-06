22bea7feb6bfc6cdf6b4b7b21ec67583ac5db481
young-jin-son <sdudwls128@naver.com>
2025-02-06 16:21:37 +0900

    refactor: OnboardingProvider 초기값 및 데이터 구조 변경 #41

--- frontend/src/pages/OnboardingPage/contexts/OnboardingProvider.tsx
+++ frontend/src/pages/OnboardingPage/contexts/OnboardingProvider.tsx
@@ -1,18 +1,26 @@
 import { useState } from 'react';
 
+import { ONBOARDING_FIRST_STEP } from '../OnboardingPage.constants';
 import { OnboardingUserInfoType } from '../OnboardingPage.types';
 import { OnboardingContext } from './OnboardingContext';
 
 export const OnboardingProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [onboardingStep, setOnboardingStep] = useState<number>(1);
+  const [onboardingStep, setOnboardingStep] = useState<number>(ONBOARDING_FIRST_STEP);
+
   const [onboardingUserInfo, setOnboardingUserInfo] = useState<OnboardingUserInfoType>({
     categories: [],
-    address: { home: '', school: '' },
+    homeAddress: '',
+    homeAddressX: 0,
+    homeAddressY: 0,
+    schoolAddress: '',
+    schoolAddressX: 0,
+    schoolAddressY: 0,
     nickname: '',
   });
+
   const [onboardingStepValidity, setOnboardingStepValidity] = useState<{ [key: number]: boolean }>({
     1: false,
-    2: true,
+    2: false,
     3: false,
   });
 
