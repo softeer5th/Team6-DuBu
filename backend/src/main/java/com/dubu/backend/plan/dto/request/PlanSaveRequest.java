a962b37c9dec39273261dbc98bc470341693dae0
codrin2 <hssang127@gmail.com>
2025-02-06 17:50:17 +0900

    fix: Path 출처 RouteSearchResponseDto에서 PlanSaveRequest로 수정ㅇ #53

--- backend/src/main/java/com/dubu/backend/plan/dto/request/PlanSaveRequest.java
+++ backend/src/main/java/com/dubu/backend/plan/dto/request/PlanSaveRequest.java
@@ -1,12 +1,10 @@
 package com.dubu.backend.plan.dto.request;
 
-import com.dubu.backend.plan.dto.response.RouteSearchResponseDto;
-
 import java.util.List;
 
 public record PlanSaveRequest(
         Integer totalSectionTime,
-        List<RouteSearchResponseDto.Path> paths
+        List<PlanSaveRequest.Path> paths
 ) {
     public record Path(
             String trafficType,
