f79b5beda369bc1a6973f55754e841875dcd930a
codrin2 <hssang127@gmail.com>
2025-02-07 17:08:03 +0900

    feat: 유저의 최근 Plan 관련 데이터 조회 API 구현 #53

--- backend/src/main/java/com/dubu/backend/plan/api/PlanController.java
+++ backend/src/main/java/com/dubu/backend/plan/api/PlanController.java
@@ -1,7 +1,9 @@
 package com.dubu.backend.plan.api;
 
+import com.dubu.backend.global.domain.SuccessResponse;
 import com.dubu.backend.plan.application.PlanService;
 import com.dubu.backend.plan.dto.request.PlanSaveRequest;
+import com.dubu.backend.plan.dto.response.PlanRecentResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,8 +18,17 @@ public class PlanController {
     @PostMapping
     public void savePlan(
             @RequestAttribute("memberId") Long memberId,
-            PlanSaveRequest planSaveRequest
+            @RequestBody PlanSaveRequest planSaveRequest
     ) {
         planService.savePlan(memberId, planSaveRequest);
+    }
+
+    @GetMapping("/recent")
+    public SuccessResponse<PlanRecentResponse> getRecentPlan(
+            @RequestAttribute("memberId") Long memberId
+    ) {
+        PlanRecentResponse response = planService.findRecentPlan(memberId);
+
+        return new SuccessResponse<>(response);
     }
 }
