4de1149dfca29004cd02d3931e1cbbb0da429fb1
codrin2 <hssang127@gmail.com>
2025-02-07 17:06:46 +0900

    feat: TodoType에 진행중 타입 추가 #53

--- backend/src/main/java/com/dubu/backend/todo/entity/TodoType.java
+++ backend/src/main/java/com/dubu/backend/todo/entity/TodoType.java
@@ -1,13 +1,14 @@
 package com.dubu.backend.todo.entity;
 
 public enum TodoType {
-    SAVE, RECOMMEND, DONE, SCHEDULED;
+    SCHEDULED, SAVE, RECOMMEND, IN_PROGRESS, DONE;
 
     public static TodoType get(String type){
         return switch(type){
             case "TODAY", "tomorrow" -> TodoType.SCHEDULED;
             case "SAVE" -> TodoType.SAVE;
             case "RECOMMEND" -> TodoType.RECOMMEND;
+            case "IN_PROGRESS" -> TodoType.IN_PROGRESS;
             default -> TodoType.DONE;
         };
     }
