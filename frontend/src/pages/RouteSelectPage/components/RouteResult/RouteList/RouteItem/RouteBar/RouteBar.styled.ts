a4fff2d1992798110e353ea971be87ce67514962
young-jin-son <sdudwls128@naver.com>
2025-02-09 03:05:57 +0900

    feat: 경로 바 컴포넌트 구현 #55

---
+++ frontend/src/pages/RouteSelectPage/components/RouteResult/RouteList/RouteItem/RouteBar/RouteBar.styled.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const RouteBarContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 0.4rem;
+  background-color: ${({ theme }) => theme.colors.gray200};
+  border-radius: 0.4rem;
+  margin-bottom: 0.4rem;
+`;
+
+export const RouteItem = styled.div<{ $barWidth: number }>`
+  display: flex;
+  position: relative;
+  flex-direction: row;
+  width: ${({ $barWidth }) => $barWidth}%;
+  height: 0.4rem;
+`;
+
+export const IconWrapper = styled.div<{ $color: string }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: -1rem;
+  width: 2.4rem;
+  height: 2.4rem;
+  background-color: ${({ $color }) => $color};
+  border-radius: 0.8rem;
+`;
+
+export const RouteProgressBar = styled.div<{ $isTraffic: boolean; $color: string }>`
+  width: 100%;
+  height: 100%;
+  background: ${({ $isTraffic, $color }) => ($isTraffic ? $color : 'transparent')};
+  border-radius: 0.4rem;
+`;
