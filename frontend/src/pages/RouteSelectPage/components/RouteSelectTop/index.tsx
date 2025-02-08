6284db1db5a106ceb6f0d3edab035365acc87e88
young-jin-son <sdudwls128@naver.com>
2025-02-09 02:59:07 +0900

    feat: 경로 선택 페이지 레이아웃 상단 컴포넌트 구현 #55

---
+++ frontend/src/pages/RouteSelectPage/components/RouteSelectTop/index.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+import RouteSelectAddress from './RouteSelectAddess';
+import RouteSelectHeader from './RouteSelectHeader';
+
+const RouteSelectTop = () => {
+  return (
+    <RouteSelectTopLayout>
+      <RouteSelectHeader />
+      <RouteSelectAddress />
+    </RouteSelectTopLayout>
+  );
+};
+
+const RouteSelectTopLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+  width: 100%;
+`;
+
+export default RouteSelectTop;
