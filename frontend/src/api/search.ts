29718acc35e679e976026fe571296829796d4246
young-jin-son <sdudwls128@naver.com>
2025-02-09 01:44:01 +0900

    feat: 경로 조회API 함수 구현 #55

--- frontend/src/api/search.ts
+++ frontend/src/api/search.ts
@@ -19,3 +19,26 @@ export const getSearchAddress = async (params: { query: string }) => {
   );
   return result.data;
 };
+
+export const getRoutes = async (params: {
+  startX: number;
+  startY: number;
+  endX: number;
+  endY: number;
+}) => {
+  const urlQueryParams = new URLSearchParams();
+
+  // params 객체의 각 값을 쿼리 파라미터로 추가
+  Object.entries(params).forEach(([key, value]) => {
+    urlQueryParams.append(key, String(value)); // 값이 숫자여도 문자열로 변환해서 추가
+  });
+
+  const queryString = urlQueryParams.toString();
+  const queryParams = queryString ? `?${queryString}` : '';
+
+  const result = await fetchClient.get<SearchAddressResponse>(
+    `${API_URL.searchRoutes}${queryParams}`,
+  );
+
+  return result.data;
+};
