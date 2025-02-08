38ebeb95e521b804e92e207fce1809ed294db34f
khw7385 <khw7385@naver.com>
2025-02-08 21:35:18 +0900

    test: 리스트에서 일정 개수 랜덤 선택 테스트 #60

--- backend/src/test/java/com/dubu/backend/todo/support/RandomPerformanceTest.java
+++ backend/src/test/java/com/dubu/backend/todo/support/RandomPerformanceTest.java
@@ -3,8 +3,9 @@ package com.dubu.backend.todo.support;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
 
 public class RandomPerformanceTest {
 
@@ -27,8 +28,8 @@ public class RandomPerformanceTest {
     void testRandom(){
         long startTime = System.currentTimeMillis();
 
+        Random random = new Random();
         for(int i = 0; i < 100000000; i++){
-            Random random = new Random();
             random.nextInt(6);
         }
 
@@ -46,6 +47,45 @@ public class RandomPerformanceTest {
             ThreadLocalRandom.current().nextInt(6);
         }
 
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("시간(ms): " + (endTime - startTime));
+    }
+
+    @Test
+    @DisplayName("리스트에서 랜덤 여러 개 선택 - Collection shuffle 이용")
+    void testSelectUsingCollectionShuffle(){
+        List<Integer> intList = new ArrayList<>(IntStream.range(0, 10) //IntStream.range(0, 1000000)
+                .boxed()
+                .toList());
+
+        long startTime = System.currentTimeMillis();
+
+        Collections.shuffle(intList);
+
+        System.out.println(intList.subList(0, 4));
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("시간(ms): " + (endTime - startTime));
+    }
+
+    @Test
+    @DisplayName("리스트에서 랜덤 여러 개 선택 - ThreadLocalRandom 사용")
+    void testSelectUsingThreadLocalRandom(){
+        List<Integer> intList = new ArrayList<>(IntStream.range(0, 10)//IntStream.range(0, 1000000)
+                .boxed()
+                .toList());
+
+        Set<Integer> result = new HashSet<>();
+
+        long startTime = System.currentTimeMillis();
+
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        while(result.size() < 4){
+            result.add(random.nextInt(intList.size()));
+        }
+
+        System.out.println(result);
         long endTime = System.currentTimeMillis();
 
         System.out.println("시간(ms): " + (endTime - startTime));
