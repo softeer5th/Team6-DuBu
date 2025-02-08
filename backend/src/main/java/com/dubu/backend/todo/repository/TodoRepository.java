5f724764192c3c4eb4601152a733a78391c27254
khw7385 <khw7385@naver.com>
2025-02-08 14:53:54 +0900

    feat: TodoRepository 에서 사용자, 부모 할 일, 타입으로 Todo 조회 쿼리 추가 #46

--- backend/src/main/java/com/dubu/backend/todo/repository/TodoRepository.java
+++ backend/src/main/java/com/dubu/backend/todo/repository/TodoRepository.java
@@ -1,7 +1,9 @@
 package com.dubu.backend.todo.repository;
 
+import com.dubu.backend.member.domain.Member;
 import com.dubu.backend.todo.entity.Schedule;
 import com.dubu.backend.todo.entity.Todo;
+import com.dubu.backend.todo.entity.TodoType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -20,8 +22,11 @@ public interface TodoRepository extends JpaRepository<Todo, Long>, CustomTodoRep
     @Query("SELECT t FROM Todo t WHERE t.parentTodo = :parentTodo AND t.schedule = :schedule")
     Optional<Todo> findByParentTodoAndSchedule(@Param("parentTodo") Todo parentTodo, @Param("schedule") Schedule schedule);
 
+    @Query("SELECT t From Todo t WHERE t.member = :member AND t.parentTodo = :parentTodo AND t.type = :type")
+    Optional<Todo> findByMemberAndParentTodoAndType(@Param("member") Member member, @Param("parentTodo") Todo parentTod, @Param("type") TodoType type);
+
     @Query("SELECT t FROM Todo t WHERE t.category.id in :categoryIds")
-    List<Todo> findTodosByCategoryIds(@Param("categoryIds") List<Long> categoryIds);
+    List<Todo> findTodosByCategoryIds(@Param("categoryIds")List<Long> categoryIds);
 
     @Query("SELECT t FROM Todo t JOIN FETCH t.category WHERE t.schedule = :schedule")
     List<Todo> findTodosWithCategoryBySchedule(@Param("schedule")Schedule schedule);
