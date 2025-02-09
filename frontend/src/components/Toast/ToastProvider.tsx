ad49e6609ee4d09998082ba7e1ae2880d8b7b330
Gyuhan Park <rbgks1937@naver.com>
2025-02-10 00:25:56 +0900

    feat: 각 toast의 걸어둔 timeout으로 인한 메모리 누수를 방지하기 위해 Map 객체로 timerId 관리 #67

--- frontend/src/components/Toast/ToastProvider.tsx
+++ frontend/src/components/Toast/ToastProvider.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useState } from 'react';
+import { PropsWithChildren, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 
 import IconButton from '../Button/IconButton';
@@ -15,25 +15,38 @@ interface Toast {
 
 const ToastProvider = ({ children }: PropsWithChildren) => {
   const [toasts, setToasts] = useState<Toast[]>([]);
+  const toastTimers = useRef(new Map<number, NodeJS.Timeout>());
 
   const toast = ({ message, duration = 2000 }: { message: string; duration?: number }) => {
     const id = Date.now();
     setToasts((prev) => [...prev, { id, message, isOpen: true }]);
 
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       removeToast(id);
     }, duration);
+
+    toastTimers.current.set(id, timer);
   };
 
   const removeToast = (id: number) => {
     setToasts((prevToasts) =>
       prevToasts.map((toast) => (toast.id === id ? { ...toast, isOpen: false } : toast)),
     );
+
+    if (toastTimers.current.has(id)) {
+      clearTimeout(toastTimers.current.get(id)!);
+      toastTimers.current.delete(id);
+    }
   };
 
   const handleAnimationEnd = (id: number, isOpen: boolean) => {
     if (!isOpen) {
       setToasts((prevToasts) => prevToasts.filter((toast) => toast.id !== id));
+
+      if (toastTimers.current.has(id)) {
+        clearTimeout(toastTimers.current.get(id));
+        toastTimers.current.delete(id);
+      }
     }
   };
 
