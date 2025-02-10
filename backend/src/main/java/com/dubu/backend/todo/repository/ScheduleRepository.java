b07f9c9c36074d1e88b3dc8a00452de07c5c4508
khw7385 <khw7385@naver.com>
2025-02-10 23:14:06 +0900

    fix: CI/CD 과정에서 존재하지 않는 메소드 호출로 인한 문제 해결 #60

--- backend/src/main/java/com/dubu/backend/todo/repository/ScheduleRepository.java
+++ backend/src/main/java/com/dubu/backend/todo/repository/ScheduleRepository.java
@@ -1,9 +1,14 @@
 package com.dubu.backend.todo.repository;
 
+import com.dubu.backend.member.domain.Member;
 import com.dubu.backend.todo.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long>, CustomScheduleRepository {
+    Optional<Schedule> findScheduleByMemberAndDate(Member member, LocalDate date);
 }
