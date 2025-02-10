db3a95111ea617c0a1398b3d4bb9cc2ddcd71778
young-jin-son <sdudwls128@naver.com>
2025-02-10 10:38:53 +0900

    refactor: 경로 선택 페이지 스타일 컴포넌트 파일 분리 #55

---
+++ frontend/src/pages/RouteSelectPage/components/RouteResult/RouteList/RouteList.styled.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const RouteListLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  overflow-y: scroll;
+  padding-bottom: 10rem;
+`;
