efa26f555ec75907c6e12faa3b9e66d57cc9c126
Gyuhan Park <rbgks1937@naver.com>
2025-02-11 00:18:07 +0900

    refactor: 할 일이 없을 경우 텍스트와 UI 변경 #74

--- frontend/src/components/Icon/icons/Plus.tsx
+++ frontend/src/components/Icon/icons/Plus.tsx
@@ -5,15 +5,15 @@ import theme from '@/styles/theme';
 const Plus = (props: SVGProps<SVGSVGElement>) => {
   return (
     <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
       {...props}
     >
       <path
-        d="M18 6L6 18M6 6L18 18"
+        d="M8 3V13M3 8H13"
         stroke={props.color || theme.colors.gray950}
         strokeWidth="1.5"
         strokeLinecap="round"
