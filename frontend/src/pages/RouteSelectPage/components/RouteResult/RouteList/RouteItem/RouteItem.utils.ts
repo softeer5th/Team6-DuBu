bdb6b6d357d67f702f0e96f3b3b5c77484cb224e
young-jin-son <sdudwls128@naver.com>
2025-02-10 10:08:16 +0900

    fix: 버스 색상 변수 사용 #55

--- frontend/src/pages/RouteSelectPage/components/RouteResult/RouteList/RouteItem/RouteItem.utils.ts
+++ frontend/src/pages/RouteSelectPage/components/RouteResult/RouteList/RouteItem/RouteItem.utils.ts
@@ -1,5 +1,6 @@
 import { SUBWAY_LINES } from '@/constants/subwayLines';
 import { PathType } from '@/pages/RouteSelectPage/RouteSelectPage.types';
+import theme from '@/styles/theme';
 
 export const getPathBarWidth = (path: PathType, totalTime: number) => {
   if (path.sectionTime === 0) return 0;
@@ -8,7 +9,7 @@ export const getPathBarWidth = (path: PathType, totalTime: number) => {
 
 export const getPathColor = (path: PathType) => {
   if (path.trafficType === 'BUS') {
-    return '#9C50D3';
+    return theme.colors.Bus;
   }
   if (path.trafficType === 'SUBWAY') {
     const subwayLine = SUBWAY_LINES[path.subwayCode as keyof typeof SUBWAY_LINES];
