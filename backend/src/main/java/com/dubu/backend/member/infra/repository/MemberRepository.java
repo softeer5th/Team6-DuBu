947ee18e18bf349676ddb1db6c356ed851b3eb46
codrin2 <hssang127@gmail.com>
2025-02-11 20:53:39 +0900

    fix: findByProviderId메서드 명 findByOauthProviderId로 수정

--- backend/src/main/java/com/dubu/backend/member/infra/repository/MemberRepository.java
+++ backend/src/main/java/com/dubu/backend/member/infra/repository/MemberRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByProviderId(String providerId);
+    Optional<Member> findByOauthProviderId(String providerId);
 }
