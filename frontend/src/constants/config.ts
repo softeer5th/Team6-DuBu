d7efab372a0a8b07a462dbded9a63a8db3a150d4
Gyuhan Park <rbgks1937@naver.com>
2025-02-12 01:18:07 +0900

    refactor: 상수 파일 분리 #87

--- frontend/src/constants/config.ts
+++ frontend/src/constants/config.ts
@@ -1,1 +1,10 @@
 export const MAX_TODO_ITEM_LENGTH = 3;
+
+export const ICON_MAPPER = {
+  READING: 'Reading',
+  ENGLISH: 'English',
+  LANGUAGE: 'Language',
+  NEWS: 'News',
+  HOBBY: 'Hobby',
+  OTHERS: 'Others',
+} as const;
