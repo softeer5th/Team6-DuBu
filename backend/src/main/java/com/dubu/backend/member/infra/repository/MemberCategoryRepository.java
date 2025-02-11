81cff50b7633de49d943430bb47fb598447576c7
codrin2 <hssang127@gmail.com>
2025-02-11 15:13:26 +0900

    feat: memberId로 Category 조회 쿼리 작성 #75

--- backend/src/main/java/com/dubu/backend/member/infra/repository/MemberCategoryRepository.java
+++ backend/src/main/java/com/dubu/backend/member/infra/repository/MemberCategoryRepository.java
@@ -2,6 +2,7 @@ package com.dubu.backend.member.infra.repository;
 
 import com.dubu.backend.member.domain.Member;
 import com.dubu.backend.member.domain.MemberCategory;
+import com.dubu.backend.todo.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +10,9 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface MemberCategoryRepository extends JpaRepository<MemberCategory, Long> {
+    @Query("SELECT mc.category FROM MemberCategory mc WHERE mc.member.id = :memberId")
+    List<Category> findCategoriesByMemberId(@Param("memberId") Long memberId);
+
     @Query("SELECT mc.category.id FROM MemberCategory mc WHERE mc.member = :member")
     List<Long> findCategoryIdsByMember(@Param("member") Member member);
 
