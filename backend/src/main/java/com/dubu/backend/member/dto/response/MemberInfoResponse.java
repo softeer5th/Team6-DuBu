a0ecc0562de5367d01c329d23f9bba34a32de58b
codrin2 <hssang127@gmail.com>
2025-02-11 15:15:04 +0900

    feat: MemberInfoResponse 생성 메서드 구현 #75

--- backend/src/main/java/com/dubu/backend/member/dto/response/MemberInfoResponse.java
+++ backend/src/main/java/com/dubu/backend/member/dto/response/MemberInfoResponse.java
@@ -1,5 +1,10 @@
 package com.dubu.backend.member.dto.response;
 
+import com.dubu.backend.member.domain.Address;
+import com.dubu.backend.member.domain.Member;
+import com.dubu.backend.member.domain.enums.AddressType;
+import com.dubu.backend.todo.entity.Category;
+
 import java.util.List;
 
 public record MemberInfoResponse(
@@ -9,4 +14,25 @@ public record MemberInfoResponse(
         String homeAddress,
         String schoolAddress
 ) {
+    public static MemberInfoResponse of(Member member, List<Category> categories, List<Address> addresses) {
+        String homeAddress = addresses.stream()
+                .filter(address -> address.getAddressType() == AddressType.HOME)
+                .map(Address::getRoadAddress)
+                .findFirst()
+                .orElse(null);
+
+        String schoolAddress = addresses.stream()
+                .filter(address -> address.getAddressType() == AddressType.SCHOOL)
+                .map(Address::getRoadAddress)
+                .findFirst()
+                .orElse(null);
+
+        return new MemberInfoResponse(
+                member.getEmail(),
+                member.getNickname(),
+                categories.stream().map(Category::getName).toList(),
+                homeAddress,
+                schoolAddress
+        );
+    }
 }
