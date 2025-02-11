74efb0e711e1155d04ce76f73654348fd6f2e89e
codrin2 <hssang127@gmail.com>
2025-02-11 15:14:41 +0900

    feat: 사용자가 온보딩시 회원 상태 체크 로직 추가 #75

--- backend/src/main/java/com/dubu/backend/member/application/MemberService.java
+++ backend/src/main/java/com/dubu/backend/member/application/MemberService.java
@@ -63,15 +63,19 @@ public class MemberService {
 
     @Transactional
     public void completeOnboarding(Long memberId, MemberOnboardingRequest request) {
-        Member member = memberRepository.findById(memberId)
+        Member currentMember = memberRepository.findById(memberId)
                 .orElseThrow(() -> new MemberNotFoundException(memberId));
+
+        if (currentMember.getStatus() != Status.ONBOARDING) {
+            throw new InvalidMemberStatusException(currentMember.getStatus().name());
+        }
 
         List<MemberCategory> memberCategories = request.categories().stream()
                 .map(categoryName -> {
                     Category category = categoryRepository.findByName(categoryName)
                             .orElseThrow(() -> new CategoryNotFoundException(categoryName));
                     return MemberCategory.builder()
-                            .member(member)
+                            .member(currentMember)
                             .category(category)
                             .build();
                 })
@@ -79,11 +83,11 @@ public class MemberService {
 
         memberCategoryRepository.saveAll(memberCategories);
 
-        saveAddress(member, AddressType.HOME, request.homeAddress(), request.homeAddressX(), request.homeAddressY());
-        saveAddress(member, AddressType.SCHOOL, request.schoolAddress(), request.schoolAddressX(), request.schoolAddressY());
+        saveAddress(currentMember, AddressType.HOME, request.homeAddress(), request.homeAddressX(), request.homeAddressY());
+        saveAddress(currentMember, AddressType.SCHOOL, request.schoolAddress(), request.schoolAddressX(), request.schoolAddressY());
 
-        member.updateNickname(request.nickname());
-        member.updateStatus(Status.STOP);
+        currentMember.updateNickname(request.nickname());
+        currentMember.updateStatus(Status.STOP);
     }
 
     private void saveAddress(Member member, AddressType type, String roadAddress, Double x, Double y) {
