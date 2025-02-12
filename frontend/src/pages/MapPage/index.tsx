a9a37a062974e4418115e4d1c5aad428e49476c3
Gyuhan Park <rbgks1937@naver.com>
2025-02-12 18:46:25 +0900

    feat: 지도에서 뱃지 클릭 시 체크 상태 여부 관리 #92

--- frontend/src/pages/MapPage/index.tsx
+++ frontend/src/pages/MapPage/index.tsx
@@ -1,6 +1,11 @@
 import { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
+
+import { CATEGORY_OPTIONS } from '../EditPage/EditPage.constants';
 
 import CenterMarker from '@/assets/images/centerMarker.svg';
+import Header from '@/components/Header';
+import { CategoryType } from '@/types/filter';
 
 const MAP_ID = 'map';
 
@@ -9,6 +14,21 @@ const MapPage = () => {
     lat: 33.450701,
     lng: 126.570667,
   });
+  const [categoryFilters, setCategoryFilters] = useState({
+    READING: false,
+    ENGLISH: false,
+    LANGUAGE: false,
+    NEWS: false,
+    HOBBY: false,
+    OTHERS: false,
+  });
+
+  const handleCheckFilter = (category: CategoryType) => {
+    setCategoryFilters((prev) => ({
+      ...prev,
+      [category]: !prev[category],
+    }));
+  };
 
   const mapRef = useRef<kakao.maps.Map | null>(null);
   const markerRef = useRef<kakao.maps.Marker | null>(null);
@@ -47,7 +67,7 @@ const MapPage = () => {
       const container = document.getElementById(MAP_ID) as HTMLElement;
       const options = {
         center: new kakao.maps.LatLng(33.450701, 126.570667),
-        level: 3,
+        level: 5,
       };
 
       mapRef.current = new kakao.maps.Map(container, options);
@@ -74,7 +94,80 @@ const MapPage = () => {
     kakao.maps.load(() => initMap());
   }, []);
 
-  return <div id={MAP_ID} style={{ width: '100%', height: '100%' }} />;
+  return (
+    <div id={MAP_ID} style={{ width: '100%', height: '100%' }}>
+      <HeaderOverlay>
+        <Header>
+          <Header.Left>
+            <Header.BackButton />
+          </Header.Left>
+          <Header.Right>
+            <Header.MenuButton />
+          </Header.Right>
+        </Header>
+        <RadioBadgeWrapper>
+          {CATEGORY_OPTIONS.map((filter) => (
+            <RadioBadge
+              key={filter.value}
+              value={filter.value}
+              $isSelected={categoryFilters[filter.value]}
+              onClick={() => handleCheckFilter(filter.value)}
+              category={filter.value}
+            >
+              {filter.label}
+            </RadioBadge>
+          ))}
+        </RadioBadgeWrapper>
+      </HeaderOverlay>
+    </div>
+  );
 };
 
 export default MapPage;
+
+const HeaderOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.lightWhite30};
+  backdrop-filter: blur(4px);
+
+  z-index: 2;
+`;
+
+export const RadioBadgeWrapper = styled.div`
+  display: flex;
+  gap: 0.8rem;
+  width: 100vw;
+  overflow-x: scroll;
+  padding: 1rem;
+`;
+
+export const RadioBadge = styled.button<{ $isSelected: boolean; category: CategoryType }>`
+  ${({ theme }) => theme.fonts.caption11};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 2.2rem;
+  white-space: nowrap;
+
+  padding: 0.5rem 1.2rem;
+  border-radius: 0.8rem;
+
+  background-color: ${({ theme, $isSelected }) =>
+    $isSelected ? theme.colors.white : theme.colors.gray50};
+
+  color: ${({ theme, $isSelected, category }) =>
+    $isSelected ? theme.colors[category] : theme.colors.gray700};
+
+  outline: ${({ theme, $isSelected, category }) =>
+    $isSelected ? `0.15rem solid ${theme.colors[category]}` : 'none'};
+
+  cursor: pointer;
+
+  transition:
+    background-color 0.2s,
+    color 0.2s;
+`;
