ca9a8cf77447f7101b1a4fe9ef2dcc770631dc3b
young-jin-son <sdudwls128@naver.com>
2025-02-12 18:02:01 +0900

    feat: 카카오 로그인 버튼 클릭 시 로그인 요청 기능 추가 #82

--- frontend/src/pages/LandingPage/components/KakaoLoginButton/index.tsx
+++ frontend/src/pages/LandingPage/components/KakaoLoginButton/index.tsx
@@ -1,10 +1,15 @@
 import * as S from './KakaoLoginButton.styled';
 
+import { kakaoLogin } from '@/api/login';
 import Icon from '@/components/Icon';
 
 const KakaoLoginButton = () => {
+  const handleKakaoLogin = () => {
+    kakaoLogin();
+  };
+
   return (
-    <S.ButtonLayout>
+    <S.ButtonLayout onClick={handleKakaoLogin}>
       <S.ButtonContent>
         <Icon icon="Kakao" />
         <span>카카오 로그인</span>
