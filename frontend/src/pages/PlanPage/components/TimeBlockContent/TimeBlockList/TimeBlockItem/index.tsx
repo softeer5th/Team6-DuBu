954d41cbbeee03af044cfced1b499a29da377f78
Gyuhan Park <rbgks1937@naver.com>
2025-02-12 22:37:05 +0900

    feat: 할 일 체크 후 취소 가능하도록 구현 #87

--- frontend/src/pages/PlanPage/components/TimeBlockContent/TimeBlockList/TimeBlockItem/index.tsx
+++ frontend/src/pages/PlanPage/components/TimeBlockContent/TimeBlockList/TimeBlockItem/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useReducer, useRef, useState } from 'react';
 
 import * as S from './TimeBlockItem.styled';
 import TodoShowForm from './TodoShowForm';
@@ -18,14 +18,14 @@ interface TimeBlockItemProps {
 const TimeBlockItem = ({ todo }: TimeBlockItemProps) => {
   const { isOpen, open, close, title } = useShowTodoBottomSheet();
 
-  const [isDone, setIsDone] = useState(todo.isDone);
+  const [isDone, toggle] = useReducer((prev) => !prev, todo.isDone);
   const [isAnimating, setIsAnimating] = useState(false);
   const checkTimeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   const handleCheckTodo = () => {
     setIsAnimating(true);
     checkTimeoutRef.current = setTimeout(() => {
-      setIsDone(true);
+      toggle();
       setIsAnimating(false);
     }, TODO_CHECK_DELAY);
   };
@@ -41,10 +41,14 @@ const TimeBlockItem = ({ todo }: TimeBlockItemProps) => {
   return (
     <>
       <S.TimeBlockItem>
-        <S.CheckIconWrapper onClick={handleCheckTodo} $isDone={isDone} $isAnimating={isAnimating}>
-          <Icon icon="EmptyCheck" cursor={isDone ? 'not-allowed' : 'pointer'} />
-          <Icon icon="FilledCheck" cursor={isDone ? 'not-allowed' : 'pointer'} />
-          <Icon icon={ICON_MAPPER[todo.category]} cursor={isDone ? 'not-allowed' : 'pointer'} />
+        <S.CheckIconWrapper
+          onClick={isDone ? toggle : handleCheckTodo}
+          $isDone={isDone}
+          $isAnimating={isAnimating}
+        >
+          <Icon icon="EmptyCheck" cursor="pointer" />
+          <Icon icon="FilledCheck" cursor="pointer" />
+          <Icon icon={ICON_MAPPER[todo.category]} cursor="pointer" />
         </S.CheckIconWrapper>
         <S.TimeBlockContent onClick={open}>
           <S.TodoTitle $isDone={isDone}>{todo.title}</S.TodoTitle>
