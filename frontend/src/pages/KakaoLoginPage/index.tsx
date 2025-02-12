0f26135f24e6a5eb80ffde71808a1e62c8ceba40
young-jin-son <sdudwls128@naver.com>
2025-02-12 18:00:44 +0900

    feat: 카카오 인증 코드로 인증  요청 처리 #82

--- frontend/src/pages/KakaoLoginPage/index.tsx
+++ frontend/src/pages/KakaoLoginPage/index.tsx
@@ -1,5 +1,18 @@
+import { useEffect } from 'react';
+import { useSearchParams } from 'react-router';
+
+import useKakaoLoginMutation from './hooks/useKakaoLoginMutation';
+
 const KakaoLoginPage = () => {
-  return <div>KakaoLoginPage</div>;
+  const [searchParams] = useSearchParams();
+  const code = searchParams.get('code');
+  const { mutate } = useKakaoLoginMutation();
+
+  useEffect(() => {
+    mutate(code);
+  }, []);
+
+  return <div>Loading...</div>;
 };
 
 export default KakaoLoginPage;
