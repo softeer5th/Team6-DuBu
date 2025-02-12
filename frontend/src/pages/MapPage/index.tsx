d1139cb26f3a399906fb56867c4bbb8b1c9837ab
Gyuhan Park <rbgks1937@naver.com>
2025-02-12 15:24:39 +0900

    feat: geolocation API로 현재 위치를 받고, 현재 위치를 마커로 표시 #92

--- frontend/src/pages/MapPage/index.tsx
+++ frontend/src/pages/MapPage/index.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 
+import CenterMarker from '@/assets/images/centerMarker.svg';
+
 const MAP_ID = 'map';
 
 const MapPage = () => {
@@ -9,11 +11,39 @@ const MapPage = () => {
   });
 
   const mapRef = useRef<kakao.maps.Map | null>(null);
+  const markerRef = useRef<kakao.maps.Marker | null>(null);
 
   useEffect(() => {
-    kakao.maps.load(() => initMap());
+    const locPosition = new kakao.maps.LatLng(center.lat, center.lng);
+    mapRef.current?.setCenter(locPosition);
 
-    function initMap() {
+    const myMarker = CenterMarker;
+    const myMarkerSize = new window.kakao.maps.Size(28, 28);
+    const myMarkerOption = { offset: new window.kakao.maps.Point(0, 0) };
+
+    const myMarkerPosition = new window.kakao.maps.MarkerImage(
+      myMarker,
+      myMarkerSize,
+      myMarkerOption,
+    );
+
+    if (mapRef.current) {
+      if (markerRef.current) {
+        // 기존 마커가 있으면 위치만 업데이트
+        markerRef.current.setPosition(locPosition);
+      } else {
+        // 마커가 없으면 새로 생성
+        markerRef.current = new window.kakao.maps.Marker({
+          map: mapRef.current,
+          image: myMarkerPosition,
+          position: locPosition,
+        });
+      }
+    }
+  }, [center]);
+
+  useEffect(() => {
+    const initMap = () => {
       const container = document.getElementById(MAP_ID) as HTMLElement;
       const options = {
         center: new kakao.maps.LatLng(33.450701, 126.570667),
@@ -39,7 +69,9 @@ const MapPage = () => {
           },
         );
       }
-    }
+    };
+
+    kakao.maps.load(() => initMap());
   }, []);
 
   return <div id={MAP_ID} style={{ width: '100%', height: '100%' }} />;
