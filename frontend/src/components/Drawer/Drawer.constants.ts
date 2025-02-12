cb5ad3dad9134b2cf48ceef6483644bb4acbd77b
Gyuhan Park <rbgks1937@naver.com>
2025-02-13 01:11:03 +0900

    refactor: 주변 둘러보기 경로 추가 #92

--- frontend/src/components/Drawer/Drawer.constants.ts
+++ frontend/src/components/Drawer/Drawer.constants.ts
@@ -1,6 +1,6 @@
 // TODO: 경로 수정
 export const DRAWER_MENU = [
-  { text: '주변 둘러보기', path: '/' },
+  { text: '주변 둘러보기', path: '/map' },
   { text: '주간 통계', path: '/' },
   { text: '마이 페이지', path: '/' },
 ] as const;
