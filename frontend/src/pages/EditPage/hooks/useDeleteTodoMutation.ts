890811cde11e65247841cee7efcab4c7b2f398ab
Gyuhan Park <rbgks1937@naver.com>
2025-02-14 03:03:42 +0900

    feat: 삭제했을 때 모든 할 일 데이터 쿼리 무효화 #105

--- frontend/src/pages/EditPage/hooks/useDeleteTodoMutation.ts
+++ frontend/src/pages/EditPage/hooks/useDeleteTodoMutation.ts
@@ -1,20 +1,24 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { deleteTodo } from '@/api/todo';
+import { TODO_TYPE } from '@/constants/config';
 import { QUERY_KEY } from '@/constants/queryKey';
 
-const useDeleteTodoMutation = (dateType: string) => {
+const useDeleteTodoMutation = (dateType: 'today' | 'tomorrow' | 'route') => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: ({ todoId, planId }: { todoId: number; planId?: number }) =>
-      deleteTodo(todoId, planId),
+      deleteTodo(todoId, TODO_TYPE[dateType]),
 
     onSuccess: (_, params) => {
       if (params.planId) {
         queryClient.invalidateQueries({ queryKey: [QUERY_KEY.routeTodoList, params.planId] });
       } else {
         queryClient.invalidateQueries({ queryKey: [QUERY_KEY.todoList, dateType] });
+        queryClient.invalidateQueries({ queryKey: [QUERY_KEY.recommendLimit, dateType] });
+        queryClient.invalidateQueries({ queryKey: [QUERY_KEY.recommendAll] });
+        queryClient.invalidateQueries({ queryKey: [QUERY_KEY.favorite] });
       }
     },
   });
