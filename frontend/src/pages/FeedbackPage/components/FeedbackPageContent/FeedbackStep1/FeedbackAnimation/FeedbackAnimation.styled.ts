25a7f390468fa34651154919eb33022eef37676d
young-jin-son <sdudwls128@naver.com>
2025-02-13 14:53:24 +0900

    refactor: 주석 제거 #66

--- frontend/src/pages/FeedbackPage/components/FeedbackPageContent/FeedbackStep1/FeedbackAnimation/FeedbackAnimation.styled.ts
+++ frontend/src/pages/FeedbackPage/components/FeedbackPageContent/FeedbackStep1/FeedbackAnimation/FeedbackAnimation.styled.ts
@@ -1,10 +1,6 @@
 import styled from 'styled-components';
 
-export const AnimationContainer = styled.div<{ $progress: number }>`
-  /* opacity: ${({ $progress }) => 0.6 + ($progress / 100) * 0.4}; */
-  /* transform: scale(${({ $progress }) => 1 + ($progress / 100) * 0.4}); */
-  /* width: 24rem; */
-  /* height: 24rem; */
+export const AnimationContainer = styled.div`
   transition: all 0.3s ease;
   display: flex;
   justify-content: center;
