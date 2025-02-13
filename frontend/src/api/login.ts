93fc5c5cc31498f669e0a014305a72ad2f37ee0d
young-jin-son <sdudwls128@naver.com>
2025-02-13 11:42:20 +0900

    refactor: 카카오 로그인 인증 함수에 타입 적용 #82

--- frontend/src/api/login.ts
+++ frontend/src/api/login.ts
@@ -1,13 +1,14 @@
 import fetchClient from './fetchClient';
 
 import { API_URL } from '@/constants/url';
+import { KakaoLogin } from '@/types/auth';
 
 export const kakaoLogin = () => {
   window.location.href = API_URL.loginKakao;
 };
 
-export const kakaoLoginAuth = async (code: string) => {
-  const result = await fetchClient.post(API_URL.loginKakaoAuth, {
+export const kakaoLoginAuth = async (code: string): Promise<KakaoLogin> => {
+  const result = await fetchClient.post<KakaoLogin>(API_URL.loginKakaoAuth, {
     body: { code },
   });
 
