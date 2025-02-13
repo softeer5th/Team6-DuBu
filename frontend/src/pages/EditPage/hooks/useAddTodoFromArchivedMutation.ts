ae73192eca414f8f011dc6755701088bb45b02d2
Gyuhan Park <rbgks1937@naver.com>
2025-02-14 03:04:39 +0900

    feat: 즐겨찾기, 추천, 모든 추천에서 추가 시 setQueryData로 쿼리 캐시 갱신 #105

--- frontend/src/pages/EditPage/hooks/useAddTodoFromArchivedMutation.ts
+++ frontend/src/pages/EditPage/hooks/useAddTodoFromArchivedMutation.ts
@@ -1,9 +1,13 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-import { addTodoFromArchived } from '@/api/todo';
+import { addTodoFromArchived, RecommendTodo } from '@/api/todo';
 import { QUERY_KEY } from '@/constants/queryKey';
+import { CategoryType, DifficultyType } from '@/types/filter';
 
-const useAddTodoFromArchivedMutation = () => {
+const useAddTodoFromArchivedMutation = (
+  categoryList?: CategoryType[],
+  difficultyList?: DifficultyType[],
+) => {
   const queryClient = useQueryClient();
 
   return useMutation({
@@ -16,11 +20,55 @@ const useAddTodoFromArchivedMutation = () => {
       todoId: number;
       planId?: number;
     }) => addTodoFromArchived(dateType, todoId, planId),
-    onSuccess: (_, params) => {
-      if (params.planId) {
-        queryClient.invalidateQueries({ queryKey: [QUERY_KEY.routeTodoList, params.planId] });
+    onSuccess: (_, { todoId, planId, dateType }) => {
+      if (planId) {
+        queryClient.invalidateQueries({ queryKey: [QUERY_KEY.routeTodoList, planId] });
       } else {
-        queryClient.invalidateQueries({ queryKey: [QUERY_KEY.todoList, params.dateType] });
+        queryClient.invalidateQueries({ queryKey: [QUERY_KEY.todoList, dateType] });
+
+        // 응답값으로 쿼리 캐싱 갱신 (추천 할 일)
+        queryClient.setQueryData<RecommendTodo[]>([QUERY_KEY.recommendLimit, dateType], (old) => {
+          if (!old) return old;
+
+          return old.map((todo) => {
+            if (todo.todoId === todoId) {
+              return { ...todo, hasChild: !todo.hasChild };
+            }
+
+            return todo;
+          });
+        });
+
+        // 응답값으로 쿼리 캐싱 갱신 (즐겨찾기)
+        queryClient.setQueryData<RecommendTodo[]>([QUERY_KEY.favorite], (old) => {
+          if (!old) return old;
+
+          return old.map((todo) => {
+            if (todo.todoId === todoId) {
+              return { ...todo, hasChild: !todo.hasChild };
+            }
+
+            return todo;
+          });
+        });
+
+        if (!categoryList || !difficultyList) return;
+
+        // 응답값으로 쿼리 캐싱 갱신 (모든 추천)
+        queryClient.setQueryData<RecommendTodo[]>(
+          [QUERY_KEY.recommendAll, ...categoryList, ...difficultyList],
+          (old) => {
+            if (!old) return old;
+
+            return old.map((todo) => {
+              if (todo.todoId === todoId) {
+                return { ...todo, hasChild: !todo.hasChild };
+              }
+
+              return todo;
+            });
+          },
+        );
       }
     },
   });
