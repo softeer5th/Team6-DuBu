3664c3f89503ede43a4577fc14db3d600d83c4f0
codrin2 <hssang127@gmail.com>
2025-02-14 01:13:55 +0900

    feat: 피드백 페이지에서 완료한 할 일만 응답하도록 설정 #111

--- backend/src/main/java/com/dubu/backend/plan/dto/response/FeedbackWritePageInfoResponse.java
+++ backend/src/main/java/com/dubu/backend/plan/dto/response/FeedbackWritePageInfoResponse.java
@@ -14,6 +14,7 @@ public record FeedbackWritePageInfoResponse(
     public static FeedbackWritePageInfoResponse of(Plan plan) {
         List<Todo> allTodos = plan.getPaths().stream()
                 .flatMap(path -> path.getTodos().stream())
+                .filter(todo -> todo.isCompleted())
                 .toList();
 
         return new FeedbackWritePageInfoResponse(
