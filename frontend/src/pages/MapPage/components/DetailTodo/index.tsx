caf621141b94cf1670c369a0182cc54cfcc217f8
Gyuhan Park <rbgks1937@naver.com>
2025-02-13 18:35:00 +0900

    feat: 다른사람의 할 일을 내 즐겨찾기에 추가/삭제 기능 구현 #92

--- frontend/src/pages/MapPage/components/DetailTodo/index.tsx
+++ frontend/src/pages/MapPage/components/DetailTodo/index.tsx
@@ -1,10 +1,38 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 import * as S from './DetailTodo.styled';
 
-import { getTodoDetail } from '@/api/map';
+import { addFavoriteFromOther, deleteFavoriteFromOther, getTodoDetail } from '@/api/map';
 import Icon from '@/components/Icon';
 import { ICON_MAPPER } from '@/constants/config';
+import { QUERY_KEY } from '@/constants/queryKey';
+
+const useAddFavoriteFromOther = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ todoId, memberId }: { todoId: number; memberId: number }) => {
+      return addFavoriteFromOther(todoId);
+    },
+    onSuccess: (_, params) => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.detailTodo, params.memberId] });
+    },
+  });
+};
+
+const useDeleteFavoriteFromOther = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ todoId, memberId }: { todoId: number; memberId: number }) => {
+      return deleteFavoriteFromOther(todoId);
+    },
+
+    onSuccess: (_, params) => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.detailTodo, params.memberId] });
+    },
+  });
+};
 
 interface DetailUserTodoProps {
   memberId: number;
@@ -12,28 +40,41 @@ interface DetailUserTodoProps {
 
 const DetailUserTodo = ({ memberId }: DetailUserTodoProps) => {
   const { data } = useQuery({
-    queryKey: ['detailTodo', memberId],
+    queryKey: [QUERY_KEY.detailTodo, memberId],
     queryFn: () => getTodoDetail(memberId),
   });
+
+  const { mutate: addFavoriteFromOther } = useAddFavoriteFromOther();
+  const { mutate: deleteFavoriteFromOther } = useDeleteFavoriteFromOther();
 
   return (
     <div>
       <div>
         <S.Nickname>{data?.nickname}님이 오늘 하고 있는 일</S.Nickname>
 
-        {data?.todo.map((todo) => (
-          <S.DetailTitleItem key={todo.todoId}>
-            <S.TitleWrapper>
-              <Icon icon={ICON_MAPPER[todo.category]} />
-              <S.DetailTitle>{todo.title}</S.DetailTitle>
-            </S.TitleWrapper>
-            {todo.isSaved ? (
-              <Icon icon="FilledFavorite" cursor="pointer" />
-            ) : (
-              <Icon icon="Favorite" cursor="pointer" />
-            )}
-          </S.DetailTitleItem>
-        ))}
+        <S.DetailTodoList>
+          {data?.todo.map((todo) => (
+            <S.DetailTodoItem key={todo.todoId}>
+              <S.TitleWrapper>
+                <Icon icon={ICON_MAPPER[todo.category]} />
+                <S.DetailTitle>{todo.title}</S.DetailTitle>
+              </S.TitleWrapper>
+              {todo.isSaved ? (
+                <Icon
+                  icon="FilledFavorite"
+                  cursor="pointer"
+                  onClick={() => deleteFavoriteFromOther({ todoId: todo.todoId, memberId })}
+                />
+              ) : (
+                <Icon
+                  icon="Favorite"
+                  cursor="pointer"
+                  onClick={() => addFavoriteFromOther({ todoId: todo.todoId, memberId })}
+                />
+              )}
+            </S.DetailTodoItem>
+          ))}
+        </S.DetailTodoList>
       </div>
     </div>
   );
