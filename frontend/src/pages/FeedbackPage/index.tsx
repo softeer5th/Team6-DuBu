134dda2efe2ae425dbd2bfe796f5a2fe7532a9fc
young-jin-son <sdudwls128@naver.com>
2025-02-13 14:08:12 +0900

    refactor: 불필요한 navigate 제거 #66

--- frontend/src/pages/FeedbackPage/index.tsx
+++ frontend/src/pages/FeedbackPage/index.tsx
@@ -1,14 +1,11 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router';
 
 import FeedbackPageContent from './components/FeedbackPageContent';
 import { FeedbackProvider } from './contexts/FeedbackProvider';
 
 const FeedbackPage = () => {
-  const navigate = useNavigate();
-
   useEffect(() => {
-    navigate(`/feedback`);
+    window.history.pushState({}, '', '/feedback');
   }, []);
 
   return (
