996a5025f8c9696ddf85edca12121702823ddcc6
Gyuhan Park <rbgks1937@naver.com>
2025-02-14 19:11:48 +0900

    refactor: 쿼리 캐싱 갱신 planId와 분기처리 제거 #117

--- frontend/src/pages/EditPage/hooks/useAddTodoFromArchivedMutation.ts
+++ frontend/src/pages/EditPage/hooks/useAddTodoFromArchivedMutation.ts
@@ -1,9 +1,9 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { addTodoFromArchived, RecommendTodo } from '@/api/todo';
-import { TODO_TYPE } from '@/constants/config';
 import { QUERY_KEY } from '@/constants/queryKey';
 import { CategoryType, DifficultyType } from '@/types/filter';
+import { TodoType } from '@/types/todo';
 
 const useAddTodoFromArchivedMutation = (
   categoryList?: CategoryType[],
@@ -13,36 +13,21 @@ const useAddTodoFromArchivedMutation = (
 
   return useMutation({
     mutationFn: ({
-      tabType,
+      todoType,
       todoId,
       planId,
     }: {
-      tabType: 'today' | 'tomorrow' | 'route';
+      todoType: TodoType;
       todoId: number;
       planId?: number;
-    }) => addTodoFromArchived(TODO_TYPE[tabType], todoId, planId),
-    onSuccess: (_, { todoId, planId, tabType }) => {
-      if (planId) {
-        queryClient.invalidateQueries({ queryKey: [QUERY_KEY.routeTodoList, 'PATH', planId] });
+    }) => addTodoFromArchived(todoType, todoId, planId),
+    onSuccess: (_, { todoId, planId, todoType }) => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.todoList, todoType] });
 
-        // 응답값으로 쿼리 캐싱 갱신 (추천 할 일)
-        queryClient.setQueryData<RecommendTodo[]>(
-          [QUERY_KEY.recommendLimit, 'PATH', planId],
-          (old) => {
-            if (!old) return old;
-
-            return old.map((todo) => {
-              if (todo.todoId === todoId) {
-                return { ...todo, hasChild: !todo.hasChild };
-              }
-
-              return todo;
-            });
-          },
-        );
-
-        // 응답값으로 쿼리 캐싱 갱신 (즐겨찾기)
-        queryClient.setQueryData<RecommendTodo[]>([QUERY_KEY.favorite, 'PATH', planId], (old) => {
+      // 응답값으로 쿼리 캐싱 갱신 (추천 할 일)
+      queryClient.setQueryData<RecommendTodo[]>(
+        [QUERY_KEY.recommendLimit, todoType, planId],
+        (old) => {
           if (!old) return old;
 
           return old.map((todo) => {
@@ -52,30 +37,28 @@ const useAddTodoFromArchivedMutation = (
 
             return todo;
           });
+        },
+      );
+
+      // 응답값으로 쿼리 캐싱 갱신 (즐겨찾기)
+      queryClient.setQueryData<RecommendTodo[]>([QUERY_KEY.favorite, todoType, planId], (old) => {
+        if (!old) return old;
+
+        return old.map((todo) => {
+          if (todo.todoId === todoId) {
+            return { ...todo, hasChild: !todo.hasChild };
+          }
+
+          return todo;
         });
+      });
 
-        if (!categoryList || !difficultyList) return;
+      if (!categoryList || !difficultyList) return;
 
-        // 응답값으로 쿼리 캐싱 갱신 (모든 추천)
-        queryClient.setQueryData<RecommendTodo[]>(
-          [QUERY_KEY.recommendAll, ...categoryList, ...difficultyList, planId],
-          (old) => {
-            if (!old) return old;
-
-            return old.map((todo) => {
-              if (todo.todoId === todoId) {
-                return { ...todo, hasChild: !todo.hasChild };
-              }
-
-              return todo;
-            });
-          },
-        );
-      } else {
-        queryClient.invalidateQueries({ queryKey: [QUERY_KEY.todoList, tabType] });
-
-        // 응답값으로 쿼리 캐싱 갱신 (추천 할 일)
-        queryClient.setQueryData<RecommendTodo[]>([QUERY_KEY.recommendLimit, tabType], (old) => {
+      // 응답값으로 쿼리 캐싱 갱신 (모든 추천)
+      queryClient.setQueryData<RecommendTodo[]>(
+        [QUERY_KEY.recommendAll, ...categoryList, ...difficultyList, planId],
+        (old) => {
           if (!old) return old;
 
           return old.map((todo) => {
@@ -85,39 +68,8 @@ const useAddTodoFromArchivedMutation = (
 
             return todo;
           });
-        });
-
-        // 응답값으로 쿼리 캐싱 갱신 (즐겨찾기)
-        queryClient.setQueryData<RecommendTodo[]>([QUERY_KEY.favorite], (old) => {
-          if (!old) return old;
-
-          return old.map((todo) => {
-            if (todo.todoId === todoId) {
-              return { ...todo, hasChild: !todo.hasChild };
-            }
-
-            return todo;
-          });
-        });
-
-        if (!categoryList || !difficultyList) return;
-
-        // 응답값으로 쿼리 캐싱 갱신 (모든 추천)
-        queryClient.setQueryData<RecommendTodo[]>(
-          [QUERY_KEY.recommendAll, ...categoryList, ...difficultyList],
-          (old) => {
-            if (!old) return old;
-
-            return old.map((todo) => {
-              if (todo.todoId === todoId) {
-                return { ...todo, hasChild: !todo.hasChild };
-              }
-
-              return todo;
-            });
-          },
-        );
-      }
+        },
+      );
     },
   });
 };
