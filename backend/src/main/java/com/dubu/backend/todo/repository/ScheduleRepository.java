44c21d88b5eacc3d5f891064b9811eef646023e5
codrin2 <hssang127@gmail.com>
2025-02-14 18:21:53 +0900

    remove: 사용하지 않는 findLatestSchedule 메서드 삭제 #120

--- backend/src/main/java/com/dubu/backend/todo/repository/ScheduleRepository.java
+++ backend/src/main/java/com/dubu/backend/todo/repository/ScheduleRepository.java
@@ -1,17 +1,8 @@
 package com.dubu.backend.todo.repository;
 
-import com.dubu.backend.member.domain.Member;
 import com.dubu.backend.todo.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
-import java.time.LocalDate;
-import java.util.Optional;
 
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long>, CustomScheduleRepository {
-    @Query("SELECT s FROM Schedule s WHERE s.member = :member AND s.date = :date")
-    Optional<Schedule> findScheduleByMemberAndDate(@Param("member") Member member, @Param("date") LocalDate date);
 }
