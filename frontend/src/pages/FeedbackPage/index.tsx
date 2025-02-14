bbe5c48c79e1421e09baae61f48236b56f4a98e8
young-jin-son <sdudwls128@naver.com>
2025-02-14 09:52:12 +0900

    refactor: 피드백 페이지에 유저 상태 조회 로직 추가 #114

--- frontend/src/pages/FeedbackPage/index.tsx
+++ frontend/src/pages/FeedbackPage/index.tsx
@@ -3,7 +3,11 @@ import { useEffect } from 'react';
 import FeedbackPageContent from './components/FeedbackPageContent';
 import { FeedbackProvider } from './contexts/FeedbackProvider';
 
+import useRedirectByMemberStatus from '@/hooks/useRedirectByMemberStatus';
+
 const FeedbackPage = () => {
+  useRedirectByMemberStatus();
+
   useEffect(() => {
     window.history.pushState({}, '', '/feedback');
   }, []);
